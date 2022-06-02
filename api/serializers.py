86e4d8665247e16fc09f552b7d0fb12c5398fff4
austintigerlu <austintigerlu@gmail.com>
2022-06-02 17:17:06 -0400

    creatE

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,3 @@
-from dataclasses import fields
 from rest_framework import serializers
 from .models import Room
 
@@ -6,3 +5,8 @@ class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ('id', 'code', 'host', 'guest_can_pause', 'votes_to_skip', 'create_at')
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip')
